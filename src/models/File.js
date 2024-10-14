1d2adb0706c32cce9d6765a864b78043c79321ed
Carl <511252519@qq.com>
2024-10-15 00:20:49 +0800

    add file

--- src/models/File.js
+++ src/models/File.js
@@ -1,11 +1,24 @@
 import { Datatypes, Model } from 'sequelize'
 import sequelize from '../core/db'
 import { FOLDER } from '../const/database'
+import { v4 as uuidv4 } from 'uuid'
 
 const { INTEGER, UUID, UUIDV4, String } = Datatypes
 
 class File extends Model {
-
+    static async createFile (params) {
+        const { parentId, title, contentId, type } = params
+        const uuid = uuidv4()
+        const data = {
+            uuid,
+            parentId,
+            title,
+            contentId,
+            type,
+        }
+        const res = await File.create(data)
+        return res
+    }
 }
 
 File.init(
